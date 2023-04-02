a899475544b9722219598585a6399805e2008afc
Eugene Mammie <emammie@gmail.com>
2023-04-02 19:47:36 -0400

    Initial AVPlayer setup with URL In View Controller

--- AVPlayerExample/ViewController.swift
+++ AVPlayerExample/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
+    var videoPlayer : AVPlayer?
+    
+    static let contentURL : URL! = URL(string:"https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8")
+                                        
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+
+        let asset = AVAsset(url: ViewController.contentURL)
+        let playerItem = AVPlayerItem(asset: asset)
+        videoPlayer = AVPlayer(playerItem: playerItem)
+        let layer = AVPlayerLayer(player: videoPlayer)
+        layer.frame = self.view.bounds
+        
+        view.layer.addSublayer(layer)
+        
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        if let player = videoPlayer {
+            player.play()
+            
+        }
+    }
 
 }
 
